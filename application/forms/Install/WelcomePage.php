ad4f7defc62baad62cb360806295491aae3a5284
Johannes Meyer <johannes.meyer@netways.de>
2014-05-23 13:36:53 +0200

    Add token authentication to the installation wizard
    resolves #6135

--- application/forms/Install/WelcomePage.php
+++ application/forms/Install/WelcomePage.php
@@ -6,9 +6,24 @@
 namespace Icinga\Form\Install;
 
 use Icinga\Web\Wizard\Page;
+use Icinga\Application\Config;
+use Icinga\Web\Form\Validator\TokenValidator;
 
 class WelcomePage extends Page
 {
-    
+    public function create()
+    {
+        $this->addNote(t('Welcome to the installation of Icingaweb 2!'), 1);
+
+        $this->addElement(
+            'password',
+            'token',
+            array(
+                'required'      => true,
+                'label'         => t('Please provide a valid authentication token to start with the installation:'),
+                'validators'    => array(new TokenValidator(Config::$configDir . '/setup.token'))
+            )
+        );
+    }
 }
 // @codeCoverageIgnoreEnd
